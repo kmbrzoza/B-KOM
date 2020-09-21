dcdbcfea221a9dc2916ce3de2e24f4d769aeb2e3
Krzysztof Brzoza <kmbrzoza@gmail.com>
2020-09-21 10:56:23 +0200

    Admin - adding new product, editing product

--- B-KOM Sklep internetowy/B-KOM Sklep internetowy/Controllers/AdminController.cs
+++ B-KOM Sklep internetowy/B-KOM Sklep internetowy/Controllers/AdminController.cs
@@ -1,8 +1,12 @@
 ﻿using B_KOM_Sklep_internetowy.DAL;
+using B_KOM_Sklep_internetowy.Infrastructure;
 using B_KOM_Sklep_internetowy.Models;
+using B_KOM_Sklep_internetowy.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -73,6 +77,181 @@ namespace B_KOM_Sklep_internetowy.Controllers
             db.SaveChanges();
 
             return RedirectToAction("OrderDetails", new { id = order.OrderId });
+        }
+
+        
+        public ActionResult AddProduct()
+        {
+            if (TempData["ViewData"] != null)
+            {
+                ViewData = (ViewDataDictionary)TempData["ViewData"];
+            }
+
+            AdminAddProductViewModel vm;
+            vm = new AdminAddProductViewModel()
+            {
+                Product = new Product(),
+                MainCategories = db.MainCategories.ToList()
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddProduct(AdminAddProductViewModel model, HttpPostedFileBase productMainImg, 
+            List<HttpPostedFileBase> productImages, HttpPostedFileBase productDescritpionImg)
+        {
+            //Checking if admin added file (productMainImg) its required
+            if(productMainImg != null && productMainImg.ContentLength > 0)
+            {
+                if(ModelState.IsValid)
+                {
+                    model.Product.AddDate = DateTime.Now;
+
+                    db.Products.Add(model.Product);
+                    db.SaveChanges();
+
+                    var fileExtMainImg = Path.GetExtension(productMainImg.FileName);
+                    var fileNameMainImg = Guid.NewGuid() + fileExtMainImg;
+                    var pathMainImg = Path.Combine(Server.MapPath(AppConfig.ProductImgFolder), fileNameMainImg);
+                    productMainImg.SaveAs(pathMainImg);
+
+                    db.ProductImages.Add(new ProductImage() { ImgPath = fileNameMainImg, MainImg = true, ProductId = model.Product.ProductId });
+
+                    foreach(var img in productImages)
+                    {
+                        var fileExtImage = Path.GetExtension(img.FileName);
+                        var fileNameImage = Guid.NewGuid() + fileExtImage;
+                        var pathImage = Path.Combine(Server.MapPath(AppConfig.ProductImgFolder), fileNameImage);
+                        img.SaveAs(pathImage);
+
+                        db.ProductImages.Add(new ProductImage() { ImgPath = fileNameImage, ProductId = model.Product.ProductId });
+                    }
+
+                    var fileExtDescImg = Path.GetExtension(productDescritpionImg.FileName);
+                    var fileNameDescImg = Guid.NewGuid() + fileExtDescImg;
+                    var pathDescImg = Path.Combine(Server.MapPath(AppConfig.ProductImgFolder), fileNameDescImg);
+                    productDescritpionImg.SaveAs(pathDescImg);
+
+                    db.ProductImages.Add(new ProductImage() { ImgPath = fileNameDescImg, ProductId = model.Product.ProductId, DescriptionImg = true });
+
+                    db.SaveChanges();
+
+                    //When success
+                    return RedirectToAction("AddProductSuccess", "Admin", new { id = model.Product.ProductId });
+                }
+                else
+                {
+                    var MainCat = db.MainCategories.ToList();
+                    model.MainCategories = MainCat;
+
+                    TempData["ViewData"] = ViewData;
+                    return View(model);
+                }
+            }
+            else
+            {
+                ViewBag.ErrorImg = "Musisz dodać główne zdjęcie!";
+                //ModelState.AddModelError("", "Wskaż główne zdjęcie");
+                var MainCat = db.MainCategories.ToList();
+                model.MainCategories = MainCat;
+
+                TempData["ViewData"] = ViewData;
+                return View(model);
+            }
+        }
+
+        public ActionResult AddProductSuccess(int id)
+        {
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+
+        public ActionResult ProductDetails(int id)
+        {
+            if (TempData["ViewData"] != null)
+            {
+                ViewData = (ViewDataDictionary)TempData["ViewData"];
+            }
+
+            var product = db.Products.Find(id);
+            var mainCat = db.MainCategories.ToList();
+            AdminProductDetailsViewModel vm = new AdminProductDetailsViewModel()
+            {
+                Product = product,
+                MainCategories = mainCat
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var prodToEdit = db.Products.Find(product.ProductId);
+                if (prodToEdit != null)
+                {
+                    prodToEdit.Name = product.Name;
+                    prodToEdit.CategoryId = product.CategoryId;
+                    prodToEdit.ShortDescription = product.ShortDescription;
+                    prodToEdit.Description = product.Description;
+                    prodToEdit.Price = product.Price;
+                    prodToEdit.Promo = product.Promo;
+                    prodToEdit.PromoPrice = product.PromoPrice;
+                    prodToEdit.Recommended = product.Recommended;
+                    prodToEdit.Bestseller = product.Bestseller;
+                    prodToEdit.Hidden = product.Hidden;
+
+                    db.SaveChanges();
+                }
+                
+                return RedirectToAction("ProductDetails", new { id = product.ProductId });
+            }
+            else
+            {
+                TempData["ViewData"] = ViewData;
+                return RedirectToAction("ProductDetails", new { id = product.ProductId });
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SetMainImgProduct(Product product, HttpPostedFileBase productMainImg)
+        {
+            var productToChange = db.Products.Find(product.ProductId);
+
+            if (productMainImg != null && productMainImg.ContentLength > 0)
+            {
+                // REMOVING
+                ProductImage imageToRemove = productToChange.ProductImages.Where(c => c.MainImg == true).FirstOrDefault();
+
+                string fullPath = Path.Combine(Server.MapPath(AppConfig.ProductImgFolder), imageToRemove.ImgPath);
+                if (System.IO.File.Exists(fullPath))
+                    System.IO.File.Delete(fullPath);
+
+                db.ProductImages.Remove(imageToRemove);
+                ////
+
+                //NEW IMAGE
+                var fileExtMainImg = Path.GetExtension(productMainImg.FileName);
+                var fileNameMainImg = Guid.NewGuid() + fileExtMainImg;
+                var pathMainImg = Path.Combine(Server.MapPath(AppConfig.ProductImgFolder), fileNameMainImg);
+                productMainImg.SaveAs(pathMainImg);
+
+                db.ProductImages.Add(new ProductImage() { ImgPath = fileNameMainImg, MainImg = true, ProductId = productToChange.ProductId });
+
+                db.SaveChanges();
+
+                TempData["Success"] = "Udalo sie zmienic zdjecie";
+                return RedirectToAction("ProductDetails", new { id = productToChange.ProductId });
+            }
+            TempData["Success"] = "Error";
+            return RedirectToAction("ProductDetails", new { id = productToChange.ProductId });
         }
     }
 }
---
+++ B-KOM Sklep internetowy/B-KOM Sklep internetowy/ViewModels/AdminAddProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using B_KOM_Sklep_internetowy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace B_KOM_Sklep_internetowy.ViewModels
+{
+    public class AdminAddProductViewModel
+    {
+        public Product Product{ get; set; }
+        public List<MainCategory> MainCategories { get; set; }
+    }
+}
